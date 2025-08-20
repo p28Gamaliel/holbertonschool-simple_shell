4e5628507d6667e2d8fbe972f4c3ca49cef8ed9f
p28Gamaliel <gamaliel13@gmail.com>
2025-08-20 17:21:47 -0300

    actualizado

---
+++ pre-shell/_which.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * main - 
+ *
+ */
+int main(int ac, char **av)
+{
+	char *path = getenv("PATH");
+	char *dob = malloc(strlen(path) + 1);
+	char *direc;
+	char Path[1024];
+
+	if (!NULL || ac < 2)
+		return (1);
+
+	strcpy(dob, path);
+	direc = strtok(dob, ":");
+
+	while (direc)
+	{
+		int len = strlen(direc) + strlen(av[1]) + 2;
+
+		if (len < (int)sizeof(Path))
+		{
+			strcpy(Path, direc);
+			strcat(Path, "/");
+			strcat(Path, av[1]);
+
+			if (access(Path, X_OK) == 0)
+				printf("%s\n", Path);
+		}
+		direc = strtok(NULL, ":");
+	}
+	free(dob);
+	return (0);
+}
