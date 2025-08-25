18da920ae4a9435991bca48b934fff0a27c0eb53
p28Gamaliel <gamaliel13perez@gmail.com>
2025-08-25 02:47:46 +0000

    arreglo print

--- commands.c
+++ commands.c
@@ -124,22 +124,23 @@ char *search_command_in_path(char *path, char *command)
  */
 extern int last_exit_status;
 
-void execute_command(char **argv, char **envp)
+void execute_command(int argc, char **argv, char **envp)
 {
 	pid_t child_pid;
 	int status;
 	char *cmd_path;
-	char *Shell_name = "Dura_Shell";
+	char *shell_name = "Dura_Shell";
 
-	shell_name = argv[0];
 	if (argc < 2)
 	{
-		fprintf(stderr, "%s: No command provided\n, Shell_name");
+		fprintf(stderr, "%s: No command provided\n", shell_name);
 			return (1);
-	cmd_path = find_in_path(argv[0], envp);
+	}
+
+	cmd_path = find_in_path(argv[1], envp);
 	if (!cmd_path)
 	{
-		fprintf(stderr, "%s: 1: %s: not found\n", Shell_name, argv[0]);
+		fprintf(stderr, "%s: 1: %s: not found\n", shell_name, argv[1]);
 		exit(127);
 	}
 	child_pid = fork();
@@ -151,9 +152,11 @@ void execute_command(char **argv, char **envp)
 	}
 	if (child_pid == 0)
 	{
-		if (execve(cmd_path, argv, NULL) == -1)
+		if (execve(cmd_path, &argv[1], envp) == -1)
+		{
 			perror("execve");
-		exit(EXIT_FAILURE);
+			exit(EXIT_FAILURE);
+		}
 	}
 	else
 	{
